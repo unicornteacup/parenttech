b3a41a0d58eb480b817d88be6ad0d41d87c6ab2c
Caroline Jepps <carolinejepps@MacBook-Pro.local>
2021-01-25 20:41:47 -0800

    Added ruby for sass and updated index

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const morgan     = require('morgan');
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
   .use(morgan('dev'))
   .use(bodyParser.urlencoded({ extended: true }))
   .use("/stylesheets", sass({
@@ -15,6 +14,7 @@ express()
   debug: true,
   outputStyle: 'expanded'
   }))
+  .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
